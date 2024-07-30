e73a05ed204bf93dc25fb7015c8570f475b9584b
OtaOkpogo <otaokpogo@yahoo.com>
2024-07-30 15:52:14 +0000

    Create a function named updateUniqueItems that returns an updated map for all items with initial quantity at 1.
    It should accept a map as an argument. The map it accepts for argument is similar to the map you create in the previous task.
    
    For each entry of the map where the quantity is 1, update the quantity to 100. If updating the quantity is not possible (argument is not a map) the error Cannot process should be thrown.

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,14 +1,16 @@
-/* eslint-disable no-param-reassign */
+/**
+ * Changes the quantity of unique grocery items to 100
+ * * @param {Map<String, number>} map - A map of the name of a
+ * grocery and its quantity.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
 export default function updateUniqueItems(map) {
-  if (map instanceof Map) {
-    for (const [key, value] of map) {
-      if (value === 1) {
-        map.set(key, 100);
-      }
-    }
-
-    return map;
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
   }
-
-  throw new Error('Cannot process');
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  });
 }
